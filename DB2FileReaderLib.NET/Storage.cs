8ad9d0e8e8466f4a347dd5f74c955bf9f87dcad5
Michał Kałużny <maku@justmaku.org>
2019-04-27 23:32:49 +0200

    Move the version detection to a separate function that can be publicly accessed by library users.

--- DB2FileReaderLib.NET/Storage.cs
+++ DB2FileReaderLib.NET/Storage.cs
@@ -14,47 +14,7 @@ namespace DB2FileReaderLib.NET
 
         public Storage(Stream stream)
         {
-            DB2Reader reader;
-
-            using (stream)
-            using (var bin = new BinaryReader(stream))
-            {
-                var identifier = new string(bin.ReadChars(4));
-                stream.Position = 0;
-                switch (identifier)
-                {
-                    case "WDC3":
-                        reader = new WDC3Reader(stream);
-                        break;
-                    case "WDC2":
-                    case "1SLC":
-                        reader = new WDC2Reader(stream);
-                        break;
-                    case "WDC1":
-                        reader = new WDC1Reader(stream);
-                        break;
-                    case "WDB6":
-                        reader = new WDB6Reader(stream);
-                        break;
-                    case "WDB5":
-                        reader = new WDB5Reader(stream);
-                        break;
-                    case "WDB4":
-                        reader = new WDB4Reader(stream);
-                        break;
-                    case "WDB3":
-                        reader = new WDB3Reader(stream);
-                        break;
-                    case "WDB2":
-                        reader = new WDB2Reader(stream);
-                        break;
-                    case "WDBC":
-                        reader = new WDBCReader(stream);
-                        break;
-                    default:
-                        throw new Exception("DB type " + identifier + " is not supported!");
-                }
-            }
+            DB2Reader reader = DB2Reader.FromStream(stream);
 
             FieldInfo[] fields = typeof(T).GetFields();
 
@@ -72,9 +32,6 @@ namespace DB2FileReaderLib.NET
                 lock (this)
                     Add(row.Value.Id, entry);
             });
-
-
         }
-
     }
 }
